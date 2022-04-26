cdd4e4a76f4044b655869bd47869120426ff9c3a
rdvnabay <ridvanabay@gmail.com>
2022-04-26 16:41:48 +0300

    Content taken from site and listed

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ConsoleApp;
+
+public class Product
+{
+    public string Link { get; set; }
+    public string Title { get; set; }   
+    public string Price { get; set; }
+
+
+    public override string ToString()
+    {
+        return GetType().GetProperties()
+        .Select(info => (info.Name, Value: info.GetValue(this, null) ?? "(null)"))
+        .Aggregate(
+            new StringBuilder(),
+            (sb, pair) => sb.AppendLine($"{pair.Name}: {pair.Value}"),
+            sb => sb.ToString());
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp;
+using HtmlAgilityPack;
+using OpenQA.Selenium.Chrome;
+
+/// <summary>
+/// Proje .Net 6 ile geliştirildi.
+/// </summary>
+
+string BASE_URL = "https://www.sahibinden.com";
+
+int counter = 0;
+double totalPrice = 0;
+List<Product> products = new();
+
+
+var driverService = ChromeDriverService.CreateDefaultService();
+driverService.HideCommandPromptWindow = true;
+
+var driver = new ChromeDriver(driverService, new ChromeOptions());
+driver.Manage().Window.Maximize();
+driver.Navigate().GoToUrl(BASE_URL);
+
+var document = new HtmlDocument();
+document.LoadHtml(driver.PageSource);
+
+
+try
+{
+    // Anasayfa Vitrininde bulunan ilanlar listeye eklendi.
+    var listItem = document.DocumentNode.SelectNodes("//div[@class='uiBox showcase']//ul//li//a[@href]");
+
+    foreach (var item in listItem)
+    {
+        var documentDetail = new HtmlDocument();
+
+        var product = new Product
+        {
+            Title = item.GetAttributeValue("title", string.Empty),
+            Link = $"{BASE_URL}{item.GetAttributeValue("href", string.Empty)}"
+        };
+        products.Add(product);
+
+        //İlana açıklama ve link bilgileri alındıktan sonra fiyat bilgisini almak için "detay" sayfasına yönlendirme işlemi yapıldı.
+        driver.Navigate().GoToUrl(products[counter].Link);
+        documentDetail.LoadHtml(driver.PageSource);
+
+        //Detay sayfası üzerinden fiyat bilgisi alıdnı.
+        product.Price = documentDetail.DocumentNode.SelectSingleNode("//div[@class='classifiedInfo ']//h3").GetDirectInnerText().Trim();
+
+        counter++;
+        totalPrice += double.Parse(product.Price.Split(product.Price.Substring(product.Price.Length - 3)).First());
+
+        //txt dosyasına bin/debug klasörü altındadır.
+        var path = @"VitrinList.txt";
+        using (StreamWriter file = new(path))
+        {
+            file.WriteLine($"İlan Adı: {product.Title} - Fiyatı:{product.Price}");
+        }
+
+        Console.WriteLine(product.ToString());
+        Console.WriteLine("---------------");
+    }
+
+    var averageAmount = totalPrice / listItem.Count;
+    Console.WriteLine("Average Amount " + averageAmount);
+}
+catch (Exception)
+{
+    Console.WriteLine("An error occurred while importing content. Please try again");
+}
+
